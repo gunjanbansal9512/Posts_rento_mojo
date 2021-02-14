1401d461f2a863edf2746336025e7548063546b6
Gunjan Bansal <bansal.gunjan10@gmail.com>
2021-02-14 18:53:30 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -35,6 +35,9 @@ function App() {
 							<Route exact path="/">
 								<Users users={users.deatils} />
 							</Route>
+							<Route path="*">
+								<Users users={users.deatils} />
+							</Route>
 						</Switch>
 					</BrowserRouter>
 				</div>
